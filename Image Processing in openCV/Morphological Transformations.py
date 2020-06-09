72969b8a3a810936b71fe39f4035883af27bd64c
Abhishek Kumar <abhishek18f@iitg.ac.in>
2020-06-09 12:21:10 +0530

    erosion & dialation

--- Image Processing in openCV/Morphological Transformations.py
+++ Image Processing in openCV/Morphological Transformations.py
@@ -22,9 +22,27 @@ while(cap.isOpened()):
 
     #Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame , frame , mask = mask)
+    
+    #APPLYING MORPHOLOGICAL TRANSFORMATIONS
+    #EROSION
+    kernel = np.ones((5,5) , np.uint8)
+    #erosion basically erodes the noise and boundary points or outliners
+    #kernal is traversed over image and it converts middle point to 1 if all points inside the kernel is 1(or of same color) otherwise zero
+    erosion = cv2.erode(mask , kernel)
 
+    #DILATION
+    #it basically amplifies outliners/noises
+    #kernal is traversed over image and it converts middle point to 1 if atleast one point inside the kernel is 1(or of same color) otherwise zero
+    dilation = cv2.dilate(mask , kernel)
+
+    #OPENING
+
+    res_erode = cv2.bitwise_and(frame , frame , mask = dilation)
     cv2.imshow('frame' , frame)
     cv2.imshow('mask' , mask)
+    cv2.imshow('erosion' , erosion)
+    cv2.imshow('dilation' , dilation)
+    cv2.imshow('res_erode' , res_erode)
     cv2.imshow('masked image', res)
     k = cv2.waitKey(5) & 0xFF
     if k == ord('q'): 
